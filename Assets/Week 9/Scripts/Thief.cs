756ef15854094534b90a4af9a1ec3e47cbbc65c4
wanziche <wanziche@OA-S141-27>
2024-03-14 16:57:03 -0400

    task4a done: not sure if dash need to speed up.

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Thief : Villager
 {
+    public GameObject daggerPrefab;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    
+    protected override void Attack()
+    {
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        base.Attack();
+        Instantiate(daggerPrefab, spawnPoint1.position, spawnPoint1.rotation);
+        Instantiate(daggerPrefab, spawnPoint2.position, spawnPoint2.rotation);
+    }
+
+   
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
