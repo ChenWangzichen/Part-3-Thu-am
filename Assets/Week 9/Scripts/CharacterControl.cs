907f358091c19365bbe97496574917a4cf924b00
wanziche <wanziche@OA-S141-27>
2024-03-14 17:23:33 -0400

    task4b done

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI Character;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -15,5 +17,17 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+    private void Update()
+    {
+        if(SelectedVillager !=null)
+        {
+            Character.text = SelectedVillager.GetType().Name;
+            Character.color = Color.green;
+        }
+        else
+        {
+            Character.text = "Choose Villager";
+        }
+       
+    }
 }
